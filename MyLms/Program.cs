e051f1aae8d86e911691349a86a168a04e0cd921
kevella95 <gisellaanindyaaisha.sdigs@gmail.com>
2026-04-01 09:30:16 +0700

    Menambah class induk  guru dan siswa

--- MyLms/Program.cs
+++ MyLms/Program.cs
@@ -1,4 +1,9 @@
-﻿public class User
+﻿Siswa siswaObj = new Siswa("Bias","bias@gmail.com","12345","siswa", "XPG3");)
+Guru guruObj = new Guru("Danisa", "danisa@gmail.com", "54321", "guru", "Matematika");
+
+siswaObj.TampilkanInfo();
+siswaObj.SumbiTugas();
+public class User
 {
     public string nama;
     public string email;
@@ -19,4 +24,40 @@
         Console.WriteLine($"Nama: {nama} | Role {role}");
     }
 }
+
+class Siswa : User
+{
+    public string ClassName;
+
+    public Siswa(string nama, string email, string password, string className) : base (nama, email, password, className)
+    {
+        ClassName = className;
+    }
+    public void SumbiTugas()
+    {
+        Console.WriteLine($"{nama}dari kelas {className} sudah ngumpulkan tugas");
+    }
+}
+  
+
+class Guru : User
+{ 
+    public string Mapel;
+
+    public Guru(string nama, string email, string password, string mapel) : base (nama, email, password, mapel)
+    {
+        Mapel = mapel;
+    }
+
+    public void TampilkanInfo(string nama, string mapel)
+    {
+        Console.WriteLine($" Guru {nama} telah menilai semua tugas {mapel}");
+    }
+}
+
+
+
+
+
+
 
